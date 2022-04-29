9a6cf71d1e9d0988e486f4e64fc3ab4aee392d3b
gamermaha <maha@revolvinggames.com>
2022-04-29 11:55:46 +0500

    Latest Build with improve art

--- Assets/Scripts/Controllers/CameraConfigControls.cs
+++ Assets/Scripts/Controllers/CameraConfigControls.cs
@@ -12,27 +12,42 @@ namespace Controllers
         public Slider zTransSlider;
         public Slider xRotSlider;
         public Slider yRotSlider;
+        
         private Camera _cam;
+        private float _xTransSliderDefValue;
+        private float _yTransSliderDefValue;
+        private float _zTransSliderDefValue;
+        private float _xRotSliderDefValue;
+        private float _yRotSliderDefValue;
 
         void Start()
         {
             _cam = FindObjectOfType<Camera>();
-            xTransSlider.minValue = -20;
-            xTransSlider.maxValue = 20;
-            yTransSlider.minValue = 0;
-            yTransSlider.maxValue = 40;
-            zTransSlider.minValue = -75;
-            zTransSlider.maxValue = -25;
-            xRotSlider.minValue = -45;
-            xRotSlider.maxValue = 45;
-            yRotSlider.minValue = -45;
-            yRotSlider.maxValue = 45;
+            
+            xTransSlider.minValue = MetaData.Instance.scriptableInstance.xTransSliderMinValue ;
+            xTransSlider.maxValue = MetaData.Instance.scriptableInstance.xTransSliderMaxValue;
+            yTransSlider.minValue = MetaData.Instance.scriptableInstance.yTransSliderMinValue;
+            yTransSlider.maxValue = MetaData.Instance.scriptableInstance.yTransSliderMaxValue;
+            zTransSlider.minValue = MetaData.Instance.scriptableInstance.zTransSliderMinValue;
+            zTransSlider.maxValue = MetaData.Instance.scriptableInstance.zTransSliderMaxValue;
+            xRotSlider.minValue = MetaData.Instance.scriptableInstance.xRotSliderMinValue;
+            xRotSlider.maxValue = MetaData.Instance.scriptableInstance.xRotSliderMaxValue;
+            yRotSlider.minValue = MetaData.Instance.scriptableInstance.yRotSliderMinValue;
+            yRotSlider.maxValue = MetaData.Instance.scriptableInstance.yRotSliderMaxValue;
+            
+            _xTransSliderDefValue = MetaData.Instance.scriptableInstance.xTransSliderDefValue;
+            _yTransSliderDefValue = MetaData.Instance.scriptableInstance.yTransSliderDefValue;
+            _zTransSliderDefValue = MetaData.Instance.scriptableInstance.zTransSliderDefValue;
+            _xRotSliderDefValue = MetaData.Instance.scriptableInstance.xRotSliderDefValue;
+            _yRotSliderDefValue = MetaData.Instance.scriptableInstance.yRotSliderDefValue;
+            
+            
 
-            xTransSlider.value = PlayerPrefs.GetFloat("CamxValue", xTransSlider.minValue);
-            yTransSlider.value = PlayerPrefs.GetFloat("CamyValue", yTransSlider.minValue);
-            zTransSlider.value = PlayerPrefs.GetFloat("CamzValue", zTransSlider.minValue);
-            xRotSlider.value = PlayerPrefs.GetFloat("CamxRot", xRotSlider.minValue);
-            yRotSlider.value = PlayerPrefs.GetFloat("CamyRot", yRotSlider.minValue);
+            xTransSlider.value = PlayerPrefs.GetFloat("CamxValue", _xTransSliderDefValue);
+            yTransSlider.value = PlayerPrefs.GetFloat("CamyValue", _yTransSliderDefValue);
+            zTransSlider.value = PlayerPrefs.GetFloat("CamzValue", _zTransSliderDefValue);
+            xRotSlider.value = PlayerPrefs.GetFloat("CamxRot", _xRotSliderDefValue);
+            yRotSlider.value = PlayerPrefs.GetFloat("CamyRot", _yRotSliderDefValue);
         }
 
         void Update()
--- Assets/Scripts/Managers/GameConfig.cs
+++ Assets/Scripts/Managers/GameConfig.cs
@@ -17,6 +17,27 @@ namespace Managers
         public double cubeLength;
         public float destroyMagnetTime;
         public float diamondTimer;
+        
+        [Header("Camera Config")]
+        public float xTransSliderMinValue = -20;
+        public float xTransSliderMaxValue = 20;
+        public float xTransSliderDefValue = 10;
+        
+        public float yTransSliderMinValue = 0;
+        public float yTransSliderMaxValue = 40;
+        public float yTransSliderDefValue = 20;
+        
+        public float zTransSliderMinValue = -75;
+        public float zTransSliderMaxValue = -25;
+        public float zTransSliderDefValue = -50;
+        
+        public float xRotSliderMinValue = -45;
+        public float xRotSliderMaxValue = 45;
+        public float xRotSliderDefValue = 0;
+        
+        public float yRotSliderMinValue = -45;
+        public float yRotSliderMaxValue = 45;
+        public float yRotSliderDefValue = 0;
 
     }
 }
